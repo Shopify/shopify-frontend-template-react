c4409bff23269634d5eebfa68f49dafeff9f6718
Elana Kopelevich <elana.kopelevich@shopify.com>
2022-04-25 21:17:33 -0600

    feat: Add TitleBar, NavigationMenu, and App Bridge routing

--- Routes.jsx
+++ Routes.jsx
@@ -7,21 +7,21 @@ export default function Routes({ pages }) {
   const routes = useRoutes(pages);
   const routeComponents = routes.map(({ path, component: Component }) => (
     <Route key={path} path={path} element={<Component />} />
-  ));
+  ))
 
-  return <ReactRouterRoutes>{routeComponents}</ReactRouterRoutes>;
+  return <ReactRouterRoutes>{routeComponents}</ReactRouterRoutes>
 }
 
 function useRoutes(pages) {
   const routes = Object.keys(pages)
     .map((key) => {
       let path = key
-        .replace("./pages", "")
-        .replace(/\.(t|j)sx?$/, "")
+        .replace('./pages', '')
+        .replace(/\.(t|j)sx?$/, '')
         /**
          * Replace /index with /
          */
-        .replace(/\/index$/i, "/")
+        .replace(/\/index$/i, '/')
         /**
          * Only lowercase the first letter. This allows the developer to use camelCase
          * dynamic paths while ensuring their standard routes are normalized to lowercase.
@@ -30,22 +30,22 @@ function useRoutes(pages) {
         /**
          * Convert /[handle].jsx and /[...handle].jsx to /:handle.jsx for react-router-dom
          */
-        .replace(/\[(?:[.]{3})?(\w+?)\]/g, (_match, param) => `:${param}`);
+        .replace(/\[(?:[.]{3})?(\w+?)\]/g, (_match, param) => `:${param}`)
 
-      if (path.endsWith("/") && path !== "/") {
-        path = path.substring(0, path.length - 1);
+      if (path.endsWith('/') && path !== '/') {
+        path = path.substring(0, path.length - 1)
       }
 
       if (!pages[key].default) {
-        console.warn(`${key} doesn't export a default React component`);
+        console.warn(`${key} doesn't export a default React component`)
       }
 
       return {
         path,
         component: pages[key].default,
-      };
+      }
     })
-    .filter((route) => route.component);
+    .filter((route) => route.component)
 
-  return routes;
+  return routes
 }
