40333446dee9ce8134b03caba305dd337a49af65
Richard Powell <richard.powell@shopify.com>
2022-04-28 11:29:02 -0400

    PR Feedback:
    1. Don't mock Polaris
    2. Test that we render a ProductsCard

--- pages/index.test.jsx
+++ pages/index.test.jsx
@@ -5,27 +5,8 @@ import { mount } from "test/mount";
 import Index from "./index";
 import { ProductsCard } from "components/ProductsCard";
 
-vi.mock("@shopify/polaris", async () => {
-  const polaris = await vi.importActual("@shopify/polaris");
-  const Layout = ({ children }) => children;
-  Layout.Section = ({ children }) => children;
-
-  const Stack = ({ children }) => children;
-  Stack.Item = ({ children }) => children;
-
-  return {
-    ...polaris,
-    Layout,
-    Stack,
-    Card: ({ children }) => children,
-    Page: ({ children }) => children,
-    TextContainer: ({ children }) => children,
-    Image: () => <img />,
-  };
-});
-
 vi.mock("components/ProductsCard", () => ({
-  ProductsCard: () => <>{"<ProductsCard/>"}</>,
+  ProductsCard: () => null,
 }));
 
 it("renders links to documentation", async () => {
@@ -50,4 +31,10 @@ it("renders links to documentation", async () => {
     url: "https://shopify.dev/apps/getting-started/add-functionality",
     external: true,
   });
+});
+
+it("renders a <ProductsCard/>", async () => {
+  const component = await mount(<Index />);
+
+  expect(component).toContainReactComponent(ProductsCard);
 });
