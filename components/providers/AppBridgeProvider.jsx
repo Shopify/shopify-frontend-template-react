146a1eef0dcdda390edc02acdb4b4bc44f97e770
Richard Powell <richard.powell@shopify.com>
2022-05-19 15:26:44 -0400

    Add doc blocks to all our custom providers and hooks

--- components/providers/AppBridgeProvider.jsx
+++ components/providers/AppBridgeProvider.jsx
@@ -2,6 +2,16 @@ import { useMemo, useRef } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 import { Provider } from '@shopify/app-bridge-react'
 
+
+/**
+ * A component to configure AppBridge.
+ * @desc A thin wrapper around AppBridgeProvider. It:
+ *
+ * 1. Ensures that navigating inside the App updates the Host URL.
+ * 2. Configures the AppBridge Provider, which unlocks functionality provided by the host.
+ *
+ * See: https://shopify.dev/apps/tools/app-bridge/react-components
+ */
 export function AppBridgeProvider({ children }) {
   const location = useLocation()
   const navigate = useNavigate()
--- hooks/useAuthenticatedFetch.js
+++ hooks/useAuthenticatedFetch.js
@@ -2,6 +2,16 @@ import { authenticatedFetch } from "@shopify/app-bridge-utils";
 import { useAppBridge } from "@shopify/app-bridge-react";
 import { Redirect } from "@shopify/app-bridge/actions";
 
+/**
+ * A hook that returns an auth aware fetch function.
+ * @desc The returned fetch function will:
+ *
+ * 1. Add a `X-Shopify-Access-Token` header to the request.
+ * 2. Check response for `X-Shopify-API-Request-Failure-Reauthorize` header.
+ * 3. Redirect the user to the reauthorization URL if the header is present.
+ *
+ * @returns {Function} fetch function
+ */
 export function useAuthenticatedFetch() {
   const app = useAppBridge();
   const fetchFunction = authenticatedFetch(app);
