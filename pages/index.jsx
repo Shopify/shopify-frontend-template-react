8a018272ad3065125e4133f155fee6eb76523aae
Richard Powell <richard.powell@shopify.com>
2022-05-20 12:49:25 -0400

    Render the TitleBar on each page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import { TitleBar } from "@shopify/app-bridge-react";
 import {
   Card,
   Page,
@@ -16,6 +17,7 @@ import { ProductsCard } from "components/ProductsCard";
 export default function HomePage() {
   return (
     <Page fullWidth>
+      <TitleBar title="Tab 1" />
       <Layout>
         <Layout.Section>
           <Card sectioned>
@@ -30,41 +32,41 @@ export default function HomePage() {
                   <Heading>Nice work on building a Shopify app 🎉</Heading>
                   <p>
                     Your app is ready to explore! It contains everything you
-                    need to get started including the{" "}
+                    need to get started including the{' '}
                     <Link url="https://polaris.shopify.com/" external>
                       Polaris design system
                     </Link>
-                    ,{" "}
+                    ,{' '}
                     <Link url="https://shopify.dev/api/admin-graphql" external>
                       Shopify Admin API
                     </Link>
-                    , and{" "}
+                    , and{' '}
                     <Link
                       url="https://shopify.dev/apps/tools/app-bridge"
                       external
                     >
                       App Bridge
-                    </Link>{" "}
+                    </Link>{' '}
                     UI library and components.
                   </p>
                   <p>
                     Ready to go? Start populating your app with some sample
-                    products to view and test in your store.{" "}
+                    products to view and test in your store.{' '}
                   </p>
                   <p>
-                    Learn more about building out your app in{" "}
+                    Learn more about building out your app in{' '}
                     <Link
                       url="https://shopify.dev/apps/getting-started/add-functionality"
                       external
                     >
                       this Shopify tutorial
-                    </Link>{" "}
-                    📚{" "}
+                    </Link>{' '}
+                    📚{' '}
                   </p>
                 </TextContainer>
               </Stack.Item>
               <Stack.Item>
-                <div style={{ padding: "0 20px" }}>
+                <div style={{ padding: '0 20px' }}>
                   <Image
                     source={trophyImgUrl}
                     alt="Nice work on building a Shopify app"
@@ -80,5 +82,5 @@ export default function HomePage() {
         </Layout.Section>
       </Layout>
     </Page>
-  );
+  )
 }
--- pages/tab2.jsx
+++ pages/tab2.jsx
@@ -1,8 +1,20 @@
 import { Card, Page, Layout, TextContainer, Heading } from '@shopify/polaris'
+import { TitleBar } from "@shopify/app-bridge-react"
 
 export default function Tab2() {
   return (
     <Page fullWidth>
+      <TitleBar
+        title="Tab 2"
+        primaryAction={{
+          content: 'Primary action',
+          onAction: () => console.log('Primary action'),
+        }}
+        secondaryActions={[{
+          content: 'Secondary action',
+          onAction: () => console.log('Secondary action'),
+        }]}
+      />
       <Layout>
         <Layout.Section>
           <Card sectioned>
