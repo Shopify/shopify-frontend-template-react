31ca7b3e0e44bd0f17c0a4e3d6b549ae00c9c22f
Damian Stasik <920747+damianstasik@users.noreply.github.com>
2023-03-13 10:25:14 +0100

    Switch to @tanstack/react-query

--- components/providers/QueryProvider.jsx
+++ components/providers/QueryProvider.jsx
@@ -1,19 +1,11 @@
-import {
-  QueryClient,
-  QueryClientProvider,
-  QueryCache,
-  MutationCache,
-} from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 /**
  * Sets up the QueryClientProvider from react-query.
- * @desc See: https://react-query.tanstack.com/reference/QueryClientProvider#_top
+ * @desc See: https://tanstack.com/query/v4/docs/react/reference/QueryClientProvider
  */
 export function QueryProvider({ children }) {
-  const client = new QueryClient({
-    queryCache: new QueryCache(),
-    mutationCache: new MutationCache(),
-  });
+  const client = new QueryClient();
 
   return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
 }
