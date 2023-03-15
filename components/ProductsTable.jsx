0aa14d946ed30b23870c582c2083138cebfdbf0a
David Witt <david.witt@shopify.com>
2023-03-15 12:40:01 -0700

    Updated loading message resource

--- components/ProductsTable.jsx
+++ components/ProductsTable.jsx
@@ -56,7 +56,7 @@ export default function ProductsTable({
       <IndexTable
         resourceName={{
           singular: "Product tags",
-          plural: "Products tags",
+          plural: "Products and tags",
         }}
         itemCount={productsArray.length}
         loading={isLoading}
