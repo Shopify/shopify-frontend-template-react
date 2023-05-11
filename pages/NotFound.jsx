609b1436e19b6906c54cbea915cef572fbe00eae
Trish Rempel <trish.rempel@shopify.com>
2023-05-11 15:08:35 -0500

    Internationalize front end app template with `react-i18next`
    Update 2 translation files
    
    Add polyfill for Intl.PluralRules

--- pages/NotFound.jsx
+++ pages/NotFound.jsx
@@ -1,19 +1,15 @@
 import { Card, EmptyState, Page } from "@shopify/polaris";
+import { useTranslation } from "react-i18next";
 import { notFoundImage } from "../assets";
 
 export default function NotFound() {
+  const { t } = useTranslation();
   return (
     <Page>
       <Card>
         <Card.Section>
-          <EmptyState
-            heading="There is no page at this address"
-            image={notFoundImage}
-          >
-            <p>
-              Check the URL and try again, or use the search bar to find what
-              you need.
-            </p>
+          <EmptyState heading={t("NotFound.heading")} image={notFoundImage}>
+            <p>{t("NotFound.description")}</p>
           </EmptyState>
         </Card.Section>
       </Card>
